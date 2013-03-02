647ab9e31bcc6a3bdf3595f7aa4aef8c0bfa4c07
bob-the-dyer <bob.the.dyer@gmail.com>
2013-03-02 16:37:14 +0400

    reworking service for remote access

--- src/ru/spb/cupchinolabs/androidlocator/LocationMonitorAction.java
+++ src/ru/spb/cupchinolabs/androidlocator/LocationMonitorAction.java
@@ -1,34 +1,96 @@
 package ru.spb.cupchinolabs.androidlocator;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.ServiceConnection;
+import android.os.*;
 import android.view.View;
 import android.widget.ToggleButton;
 
 public class LocationMonitorAction extends Activity {
 
+    //TODO FIX BUG - handle navigation on back button, app shouldn't be destroyed
+
+    /**
+     * Messenger for communicating with the service.
+     */
+    private Messenger messenger;
+
+    /**
+     * Flag indicating whether we have called bind on the service.
+     */
+    private boolean isBound;
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            messenger = new Messenger(service);
+            isBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            messenger = null;
+            isBound = false;
+        }
+    };
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_monitor_control);
-        //TODO check liveness of service
-        //TODO set toggle button value
+        //TODO check liveness of service ?
+        //TODO set toggle button value ?
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bindService();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindServiceIf(isBound);
+    }
 
     public void onToggleClicked(View view) {
+        if (!isBound) {
+            bindService();
+        }
         boolean on = ((ToggleButton) view).isChecked();
-        Intent intent = new Intent(this, LocationMonitorService.class);
+        int messageId;
         if (on) {
-            startService(intent);
+            messageId = LocationMonitorService.MSG_START;
         } else {
-            stopService(intent);
+            messageId = LocationMonitorService.MSG_STOP;
+        }
+        Message msg = Message.obtain(null, messageId, 0, 0);
+        try {
+            messenger.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        unbindServiceIf(!on);
+
+    }
+
+    private void unbindServiceIf(boolean unbind) {
+        if (unbind) {
+            unbindService(serviceConnection);
+            isBound = false;
         }
     }
 
-    //TODO startService
-    //TODO stopService
-    //bindService() - onStartCommand() is not called
-    //unbindService()
+    private void bindService() {
+        bindService(new Intent(this, LocationMonitorService.class), serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
 }
--- src/ru/spb/cupchinolabs/androidlocator/LocationMonitorService.java
+++ src/ru/spb/cupchinolabs/androidlocator/LocationMonitorService.java
@@ -4,7 +4,10 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.widget.Toast;
 
 /**
@@ -15,38 +18,52 @@ import android.widget.Toast;
  */
 public class LocationMonitorService extends Service {
 
-    private static final int ONGOING_NOTIFICATION = 135438438;
+    protected static final int MSG_START = 1;
+    protected static final int MSG_STOP  = 2;
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-    // The system calls this method when another component, such as an activity, requests that the service be started,
-    // by calling startService()
-        return START_STICKY;
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final private Messenger messenger = new Messenger(new IncomingHandler());
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    private class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_START:
+                    notifyOnStart();
+                    //TODO start worker thread (see HandlerThread, Looper)
+                    //TODO replace Toast with notification
+                    break;
+                case MSG_STOP:
+                    notifyOnStop();
+                    //TODO stop worker thread
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
     }
 
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
     @Override
     public IBinder onBind(Intent intent) {
-//        The system calls this method when another component wants to bind with the service (such as to perform RPC),
-//        by calling bindService().In your implementation of this method, you must provide an
-//        interface that clients use to communicate with the service,by returning an IBinder.You must always implement
-//        this method, but if you don 't want to allow binding, then you should return null.
-        return null;
+        Toast.makeText(this, "Location monitor service - binding ...", Toast.LENGTH_SHORT).show();
+        return messenger.getBinder();
     }
 
     @Override
     public void onCreate() {
 //        The system calls this method when the service is first created, to perform one -time setup procedures (before
 //        it calls either onStartCommand () or onBind ()).If the service is already running, this method is not called.
-        Notification notification = new Notification(R.drawable.stat_happy, getText(R.string.locator_notification_ticker_text),
-                System.currentTimeMillis());
-        Intent notificationIntent = new Intent(this, LocationMonitorAction.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-        notification.setLatestEventInfo(this, getText(R.string.locator_notification_content_title),
-                getText(R.string.locator_notification_content_text), pendingIntent);
-        startForeground(ONGOING_NOTIFICATION, notification);
-
-        //TODO start worker thread (see HandlerThread, Looper)
-        Toast.makeText(this, "Location monitor service is starting ...", Toast.LENGTH_SHORT).show();
+        //TODO init ContentProvider, check wifi, gps?
+        Toast.makeText(this, "Location monitor service - creating ...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -54,8 +71,24 @@ public class LocationMonitorService extends Service {
 //        The system calls this method when the service is no longer used and is being destroyed.Your service
 //        should implement this to clean up any resources such as threads, registered listeners, receivers, etc.This
 //        is the last call the service receives.
+        //TODO release all the resources
+        Toast.makeText(this, "Location monitor service - destroying ...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void notifyOnStop() {
+        //TODO replace Toast with notification
         Toast.makeText(this, "Location monitor service is stopping ...", Toast.LENGTH_SHORT).show();
         stopForeground(true);
-        //TODO stop worker thread
+    }
+
+    private void notifyOnStart() {
+        Notification notification = new Notification(R.drawable.stat_happy, getText(R.string.locator_notification_ticker_text),
+                System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, LocationMonitorAction.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(this, getText(R.string.locator_notification_content_title),
+                getText(R.string.locator_notification_content_text), pendingIntent);
+        startForeground(R.string.locator_notification_content_title, notification);
+        Toast.makeText(this, "Location monitor service is starting ...", Toast.LENGTH_SHORT).show();
     }
 }
